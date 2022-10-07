a273ebd2ec23daa5ed7a5e72b8ef0f9eae3b7e49
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-10-07 15:20:30 -0300

    update regra de negocio
    nova regra de negocio implementada

--- src/service/ReajusteService.java
+++ src/service/ReajusteService.java
@@ -3,6 +3,7 @@ package service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import sistema.Funcionario;
 import sistema.ValidacaoException;
@@ -14,7 +15,13 @@ public class ReajusteService {
 		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
 		}
-
+		//nova regra dos 6 meses
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		long mesesDeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		if (mesesDeUltimoReajuste < 6) {
+			throw new ValidacaoException("Intervalo entre reajuste deve ser no min 6 meses!");
+		}
 		BigDecimal salarioReajustado = salarioAtual.add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
 	}
